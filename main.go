0e172bf8297e808bf61e44c3238749384d7bb055
Gemmingen <edwelllouis@gmail.com>
2026-09-04 15:26:35 +0200

    .

--- main.go
+++ main.go
@@ -43,7 +43,10 @@ func main() {
 	// Swagger UI
 	e.Static("/openapi.yaml", "openapi.yaml")
 	e.GET("/swagger", func(c echo.Context) error {
-		html := `<!DOCTYPE html><html><head><link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" /></head><body><div id="swagger-ui"></div><script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script><script>window.onload = () => { window.ui = SwaggerUIBundle({ url: '/openapi.yaml', dom_id: '#swagger-ui' }); };</script></body></html>`
+		html := `<!DOCTYPE html><html><head><link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+		// </head><body><div id="swagger-ui"></div><script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js">
+		// </script><script>window.onload = () => { window.ui = SwaggerUIBundle({ url: '/openapi.yaml', dom_id: '#swagger-ui' }); };
+		// </script></body></html>`
 		return c.HTML(200, html)
 	})
 
